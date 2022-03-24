46dcc024821c3f2098aa7bcfcd912319efb94617
bmperrien <97481759+bmperrien@users.noreply.github.com>
2022-03-24 09:27:55 -0500

    Add files via upload

---
+++ src/main/java/com/revature/azurepractice/AzurepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.revature.azurepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurepracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurepracticeApplication.class, args);
+	}
+
+}
